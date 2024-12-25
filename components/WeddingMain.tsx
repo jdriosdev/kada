0a28223a3464110635bb8f07c8a48e426d6b5d15
Daniel Rios <jd.rios.dev@gmail.com>
2024-12-25 13:28:22 +1100

    update date

--- components/WeddingMain.tsx
+++ components/WeddingMain.tsx
@@ -15,8 +15,8 @@ function WeddingMain() {
     text='Two hearts, one love, and a lifetime together. You are invited!'
     />
     <Names isBig/>
-    <p className={`${quicksand.className}`}>xx | March | 2025</p>
-    <p className={`${quicksand.className}`}>3 PM</p>
+    <p className={`${quicksand.className}`}>22 | March | 2025</p>
+    <p className={`${quicksand.className}`}>2 PM</p>
     <p className={`${quicksand.className}`}>CGMJI - Sydney</p>
     <p className={`${quicksand.className}`}>Sydney</p>
     <Spacer/>
